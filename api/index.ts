fab27f81dc1a2ddab4c23feeb9d07412bfb672d0
Chokladglasyr <lim_ida@live.com>
2025-08-21 11:47:49 +0200

    feat: added deployed link to cors

--- api/index.ts
+++ api/index.ts
@@ -2,6 +2,7 @@ import express, {Express, Request, Response} from "express";
 import dotenv from "dotenv";
 import connectDB from "../database/db";
 import attendeeRouter from "../routes/attendeeRoutes";
+import cors from "cors";
 
 
 const app: Express = express();
@@ -10,6 +11,11 @@ const PORT: string | number = 3006
 dotenv.config();
 connectDB();
 app.use(express.json());
+
+app.use(cors({
+    origin: ["https://hyllnings-match.netlify.app/", "http://localhost:5173"], 
+    credentials: true
+}));
 
 app.get("/", (req: Request, res: Response) => {
     res.send("For Nils")
